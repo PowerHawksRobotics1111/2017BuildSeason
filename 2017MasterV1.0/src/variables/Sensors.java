647c77dda669086a91a1fddee82a8df30ddca128
Max <mxmeinhold@gmail.com>
2017-01-15 01:09:31 -0500

    Update Sensors.java

--- 2017MasterV1.0/src/variables/Sensors.java
+++ 2017MasterV1.0/src/variables/Sensors.java
@@ -5,6 +5,6 @@ import edu.wpi.first.wpilibj.Encoder;
 
 public class Sensors {
 
-	//initialize encoders using format Encoder(channelA, channelB)
+	//initialize encoders using format Encoder(channelA, channelB), unless it is connected to the talon, in which case you use the talon's method.
 	Encoder leftDriveEnc = new Encoder(null, null);
 }
