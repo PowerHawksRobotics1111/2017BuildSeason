5d67d62319c8ea19731c9bae901cda47cddd5f8a
Max <mxmeinhold@gmail.com>
2017-01-15 00:26:27 -0500

    Update motor rotation direction.

--- 2017MasterV1.0/src/subsystems/Drivetrain.java
+++ 2017MasterV1.0/src/subsystems/Drivetrain.java
@@ -8,10 +8,10 @@ public class Drivetrain
 	public void drive(double left, double right)
 	{
 		Motors.motorDriveFrontRight.set(right);
-		Motors.motorDriveFrontLeft.set(left);
+		Motors.motorDriveFrontLeft.set(-left);
 		Motors.motorDriveBackRight.set(right);
-		Motors.motorDriveBackLeft.set(left);	}
+		Motors.motorDriveBackLeft.set(-left);
+	}
 		
-	
 }
 
